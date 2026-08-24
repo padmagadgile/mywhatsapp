8633d2a3ef73b1ed1f3d85b2a883b4b416a5dcc8
Padma <gadgilepadma@gmail.com>
2026-08-24 21:24:33 +0530

    Add SQLite database

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
+
+DATABASE = "chat.db"
+
+
+def init_db():
+    connection = sqlite3.connect(DATABASE)
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL
+        )
+    """)
+
+    connection.commit()
+    connection.close()
 
 
 @app.route("/")
@@ -9,4 +29,5 @@ def home():
 
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
